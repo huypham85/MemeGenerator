81e84ac87a454d68bd1d8429a55554741b1edad7
HuyPT3 <huypt3@rikkeisoft.com>
2024-11-14 11:34:02 +0700

    Initial Commit

---
+++ MemeGenerator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemeGenerator
+//
+//  Created by HuyPT3 on 14/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MemeGenerator/MemeGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeGeneratorApp.swift
+//  MemeGenerator
+//
+//  Created by HuyPT3 on 14/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemeGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
